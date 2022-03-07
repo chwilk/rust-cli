275add390cb7b489d1e6bf3e595c77a1dc1700b5
chwilk <chwilk@gmail.com>
2022-03-07 13:49:46 -0600

    Passing tests ch6

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,8 +1,10 @@
-use std::error::Error;
-use std::fs::File;
-use std::io::Read;
-use std::io::{self, BufRead, BufReader};
 use clap::{App, Arg};
+use std::{
+    error::Error,
+    fs::File,
+    io::Read,
+    io::{self, BufRead, BufReader, Write, BufWriter},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -47,7 +49,50 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let mut file = open(&config.in_file)
+        .map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut out_file = open_out(config.out_file)
+        .map_err(|e_o| format!("{}", e_o))?;
+    let mut line = String::new();
+    let mut last = String::new();
+    let mut count = 0;
+    let mut print = |count: u64, text: &str| -> MyResult<()>{
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 { break; }
+        if line.trim_end() == last.trim_end() {
+            count += 1;
+        } else {
+            print(count, &last);
+            count = 1;
+            last.clone_from(&line);
+        }
+        line.clear();
+    }
+    print(count, &last);
 
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn open_out(out_file: Option<String>) -> MyResult<Box<dyn Write>> {
+    match out_file {
+        Some(filename) => Ok(Box::new(BufWriter::new(File::create(filename)?))),
+        None => Ok(Box::new(BufWriter::new(io::stdout()))),
+    }
 }
